e5b8a559c28bd33fc1a0ed28f304b3a069d09d38
Eswitala fmmf <eswitalafmd@gmail.com>
2025-11-17 18:07:30 +0100

    dom

---
+++ 04-Functions/alg.py
@@ -0,0 +1,13 @@
+###
+# Calculates the sum of the digits in a number
+#
+def sum_digits(number):
+    number = input('write your number here. please.')
+    abs_val = abs(number)
+    str_val = str(abs_val)
+    
+    return sum_digits
+
+any_number = int(input('Enter integer number: '))
+result = sum_digits(...)
+print('The sum of the digits in the number ... is ...')
