359eddf58124ff91ecb86780420b4be881e697fc
Eswitala fmmf <eswitalafmd@gmail.com>
2025-11-17 17:31:23 +0100

    heeeellll

---
+++ 04-Functions/fun.py
@@ -0,0 +1,14 @@
+###
+# To use the functions available in an external module,
+# you must include that module in your program.
+import math
+
+square_root = math.sqrt(7)
+print('A square root of 7 is', square_root)
+
+nat_logarithm = math.log1p(4)
+print('A natural logarithm of 5 is', nat_logarithm)
+
+ang = math.radians(90)
+sine = math.sin(ang)
+print('The sine of 90 degrees is', sine)
