a0594e1558acc34a11a14b226228978655279697
Eswitala fmmf <eswitalafmd@gmail.com>
2025-11-17 17:51:33 +0100

    hell yeah!!

---
+++ 04-Functions/heron.py
@@ -0,0 +1,15 @@
+###
+# Calculates the area of a triangle based on the lengths
+# of the triangle's sides
+#
+import math
+def triangle_area(a,b,c):
+    p = (a + b + c)/2
+    result1 = math.sqrt(p * (p-a) * (p-b) * (p-c))
+    return result1
+area1 = triangle_area(3,4,5)
+area2 = triangle_area(5,12,13)
+area3 = triangle_area(7,24,25)
+print(f'The area of ​​a triangle with sides 3, 4, 5 is {area1}')
+print(f'The area of ​​a triangle with sides 5, 12, 13 is {area2}')
+print(f'The area of ​​a triangle with sides 7, 24, 25 is {area3}')
