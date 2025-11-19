4100862c063f69628eea47e179f9036f0b7ca9ac
emilswitala <eswitalafmd@gmail.com>
2025-11-19 10:58:51 +0100

    hello world

--- 02-TypesAndVariables/5-5.py
+++ 02-TypesAndVariables/5-5.py
@@ -15,6 +15,7 @@ dropped_price = (price * (100 - discount))/100
 price = round(price,2)
 dropped_price = round(dropped_price,2)
 diff = price - dropped_price 
+diff = round(diff,2)
 
 
 print(f"The price of this product before was {price} PLN. A discount of {discount}% was applied. Your new price is {dropped_price} PLN. You have saved {diff} PLN.")
---
+++ 02-TypesAndVariables/6-4.py
@@ -0,0 +1,9 @@
+###
+# A program for printing detailed information.
+# "Mr. John May, born on 1998-02-16"
+
+employee = "Mr. John May, born on 1998-02-16"
+print(f'Name: {employee[4:8]}')
+print(f'Surname: {employee[9:12]}')
+print(f'Born: {employee[22:33]}')
+print(f'Initials: {employee[4]}{employee[9]}')
