5c9f28590d8a7641997ec500a1e217829cd62acb
emilswitala <eswitalafmd@gmail.com>
2025-11-20 17:19:34 +0100

    commit

--- 03-ControlStructures/5/5-6.py
+++ 03-ControlStructures/5/5-6.py
@@ -3,12 +3,12 @@
 #
 N = 10
 sum_even = 0
-number = range(1,N+1)
+i = 1
 # Calculate the sum of even numbers
 
-while True:
-    number = range(1,N+1)
-    if number % 2 == 0:
-        sum_even += number
+while i <= N + 1:
+    if i % 2 == 0:
+        sum_even += i
+    i += 1
 
 print(f"The sum of even numbers from 1 to {N} is: {sum_even}")
