a3cc89a7889f4c519807e06fdecd4432cabe6a7b
emilswitala <eswitalafmd@gmail.com>
2025-11-20 11:06:18 +0100

    COMMIT

--- 02-TypesAndVariables/6-8.py
+++ 02-TypesAndVariables/6-8.py
@@ -10,7 +10,9 @@ last = input('Enter your second letter: ')
 
 first_letter_code = ord(first)
 last_letter_code = ord(last)
-
-number_of_letters = first_letter_code - last_letter_code - 1
+if first_letter_code != last_letter_code:
+    number_of_letters = last_letter_code - first_letter_code - 1
+else:
+    number_of_letters = 0
 
 print(f'Between {first} and {last} is {number_of_letters} letters')
