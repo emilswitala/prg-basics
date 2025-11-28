57ba87f1e8728fe8800ce736d8183865d57ac928
Emilswitala <eswitalafmd@gmail.com>
2025-11-28 15:22:33 +0100

    person = {    "name": "Marek",    "surname": "Banach",    "age": 25,    "hobby": ["swimming","excursions"],    "married": True,    "phone":{"landline":"123444321","mobile":"777888999"} }

---
+++ 09-DictionariesStacksAndQueues/1/1-3.py
@@ -0,0 +1,10 @@
+mobile = {
+"OS": "Android",
+"colour": "black",
+"model": "A32",
+"system update":"1 Sep 2025",
+"vibes":"ok",
+"ASPL":"2"
+}
+for key,value in mobile.items():
+   print(f"{key} : {value}")
