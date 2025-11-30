72ef5abcff4f9607384227ab1fa8d78fb17f6534
emilswitala <eswitalafmd@gmail.com>
2025-11-30 23:58:14 +0100

    poepoepoe

--- 03-ControlStructures/6/6-functions/ugh.py
+++ 03-ControlStructures/6/6-functions/ugh.py
@@ -11,7 +11,7 @@ text = 'I am so very very awesome el oh el'
 def f(text):
     new = text.split()
     result = ""
-    for x in range(0,len(new)):
+    for x in range(len(new)):
         result += new[x][:1]
     return result
 
